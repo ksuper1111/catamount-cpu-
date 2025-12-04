b27a1a3e6f2e3e01759d54eecc5e01ab5c9d6286
kalibanghart <kabanghar@uvm.edu>
2025-12-04 14:16:35 -0500

    added assembly code

--- multiply_p2_loop.asm
+++ multiply_p2_loop.asm
@@ -1,14 +1,14 @@
-        LOADI  R0, 0x0001   ; 1
-        LOADI  R1, 0x0000   ; address
-        LOADI  R2, 0x0009   ; count (9 values)
-        LOADI  R3, 0x0005   ; current = 5
+        LOADI  R0, 0x0001
+        LOADI  R1, 0x0000
+        LOADI  R2, 0x0009
+        LOADI  R3, 0x0005
 
-loop:   ADD    R3, R3, R3   ; current = current * 2
+ loop:   ADD    R3, R3, R3
 
-        STORE  R3, [R1]     ; MEM[address] = current
-        ADD    R1, R1, R0   ; address++
+        STORE  R3, [R1]
+        ADD    R1, R1, R0
 
-        SUB    R2, R2, R0   ; count--
-        BNEZ   R2, loop     ; or your ISA's "branch if not zero"
+        SUB    R2, R2, R0
+        BNEZ   R2, loop
 
         HALT
