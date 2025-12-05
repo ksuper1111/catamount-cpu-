a38dd108576eb61dc986f733ba38b065de7288c7
rlstutz <stutzmanriley@gmail.com>
2025-12-04 22:51:51 -0500

    commiting

--- asm/add_and_or.asm
+++ asm/add_and_or.asm
@@ -6,7 +6,7 @@ AND R3, R0, R1      // (R0 & R1) stored in R3
 BEQ R3, #0, safe    // if (R0 & R1) = 0 the addition is safe
 
 unsafe:
-    LOADI R2, #0    // loads 0 into R2 when the addition is  unsafe
+    LOADI R2, #0    // loads 0 into R2 when the addition is  unsafepyt
     HALT
 
 safe:
--- asm/divide_p2.asm
+++ asm/divide_p2.asm
@@ -2,7 +2,13 @@
 LOADI R0, #0xC0
 LUI R0, #0x01       // the original constant operand, decimal 448
 LOADI R1, #0x06
-LUI R1, #0x80     // last operand used by SHFT
+LUI R1, #0x80
+
+ADDI  R0, R0, #0
+ADDI  R0, R0, #0
+ADDI  R0, R0, #0
+ADDI  R0, R0, #0
+ADDI  R0, R0, #0
 
 SHFT R2, R0, R1     // (448 / 2^1)
 SHFT R3, R2, R1     // (448 / 2^2)
@@ -12,3 +18,4 @@ SHFT R6, R5, R1     // (448 / 2^5)
 SHFT R7, R6, R1     // (448 / 2^6)
 
 HALT
+
